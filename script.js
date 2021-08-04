8ac1d1dcb8d42576aa1e175cdee3b27fc669f264
Vyacheslav <mister.slaus@gmail.com>
2021-08-04 10:50:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', function() {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        
+        document.body.style.overflow = '';
+    }
+    
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) { 
+            closeModal();
+        }
+    });
+});
